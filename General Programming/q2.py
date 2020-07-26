a957f066ce65667107f23d11a80de5700ca5eb3d
Nathan Strickland <nathanstrickland@Nathans-MacBook-Pro.local>
2020-07-26 16:58:57 +0100

    Removed duplicate question in q2 general programming

--- General Programming/q2.py
+++ General Programming/q2.py
@@ -23,13 +23,3 @@ def find_common_values(nums_1, nums_2):
         else:
             second += 1
     return common_values
-
-
-# Time complexity O(logN) where N is number of digits, Space complexity O(1)
-def even_digits(n):
-    while n > 0:
-        digit = n % 10
-        if digit % 2 != 0:
-            return False
-        n //= 10
-    return True
